c80301bca6a7f9e574ab1ac5be1f984436ea2725
klar <a+git@pxd.ch>
2018-10-01 13:22:15 +0200

    sql file auto insert to db container, Readme change, compose change without volumes

--- db/create.sql
+++ db/create.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `theca` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE `theca`; 
 
 CREATE TABLE `theca`.`opreturn` (
   `txid` VARCHAR(64) NOT NULL,
