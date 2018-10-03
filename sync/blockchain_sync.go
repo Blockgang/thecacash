ac566fb6bce836853a62a4290b021ee83766785d
Your Name <you@example.com>
2018-10-03 22:23:54 +0200

    update

--- sync/blockchain_sync.go
+++ sync/blockchain_sync.go
@@ -77,6 +77,7 @@ func insertIntoMysql(TxId string, prefix string, hash string, data_type string, 
 }
 
 func main() {
+	time.Sleep(10 * time.Second)
 	var err error
 	db, err = sql.Open("mysql", "root:8drRNG8RWw9FjzeJuavbY6f9@tcp(192.168.12.2:3306)/theca")
 	if err != nil {
